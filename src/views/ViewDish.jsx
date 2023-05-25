f5f510d23318a78beedb09a1c0b004a0fa84076b
DavidAjanaku <davidajanaku46163@gmail.com>
2023-05-25 17:08:31 +0100

    Done

--- src/views/ViewDish.jsx
+++ src/views/ViewDish.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import CarouselImageGallery from "../components/CarouselImageGallery";
+import leftarrow from "/assets/left arrow.png";
+import starRating from "/assets/preference.png";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 const ViewDish = () => {
   const { id } = useParams(); // Retrieve the id (index) from the URL parameter
@@ -9,16 +13,72 @@ const ViewDish = () => {
   const dish = CarouselImageGallery.CarouselImageGallery[Number(id)];
 
   return (
-    <div>
-      {dish ? (
-        <div>
-          <h1>{dish.name}</h1>
-          <img src={dish.imageURL} alt={dish.name} />
-          <p>Rating: {dish.rating}</p>
+    <div className="h-[90vh] overflow-scroll  md:h-[100vh] md:w-5/6  mx-auto">
+      <div className="p-4">
+        <div className="flex justify-between">
+          <button
+            onClick={() => {
+              closeModal(false);
+            }}
+          >
+            <img src={leftarrow} className="w-4" alt="" />
+          </button>
+
+          <button className="text-sm">clear all</button>
         </div>
-      ) : (
-        <p>No dish found.</p>
-      )}
+        {dish ? (
+          <div>
+            <div className="h-72 md:h-2/4">
+              <img
+                src={dish.imageURL}
+                alt={dish.name}
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div className="overflow-scroll   meda  overscroll-contain  -mt-3">
+              <div className=" bg-lemon-meringue inset-0 p-4 rounded-t-3xl  overflow-y-auto overscroll-contain">
+                <h1 className="text-2xl text-center font-medium">{dish.name}</h1>
+
+                <div className="flex justify-center justify-evenly mt-4 flex-wrap">
+                  <span className="flex items-center  ">
+                    {" "}
+                    <img src={starRating} className="w-4" alt="" />{" "}
+                    {dish.rating}
+                    &nbsp;
+                    &nbsp; <br />
+                  <span className="text-sm text-cyan-400"> 
+                  <Link>
+                    25 reviews
+                    </Link>
+                  </span>
+                  </span>
+                  <p className="uppercase"> {dish.level}</p>
+                  <p> {dish.time} min</p>
+                  <p>Servings: {dish.servings}</p>
+
+                </div>
+                
+                <div className="mt-4">
+                  <h1 className="text-2xl font-medium">Ingredients</h1>
+                  <ul>
+                    {dish.ingredients.map((ingredients, index) => (
+                      <li key={index} className="flex justify-between">
+                        <span>{ingredients.name}</span>
+                        <span>{ingredients.quantity}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  
+                </div>
+                <Button title="Show directions"/>
+
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p>No dish found.</p>
+        )}
+      </div>
     </div>
   );
 };
