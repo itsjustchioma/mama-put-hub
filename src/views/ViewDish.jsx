1c01673ef0b5a61a8a11c1980c86a9dc1994b960
DavidAjanaku <davidajanaku46163@gmail.com>
2023-05-25 17:30:04 +0100

    done

--- src/views/ViewDish.jsx
+++ src/views/ViewDish.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import CarouselImageGallery from "../components/CarouselImageGallery";
-import leftarrow from "/assets/left arrow.png";
 import starRating from "/assets/preference.png";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import fullBookmarkIcon from "/public/assets/fullbookmark.png";
+
+import BackArrow from "../components/BackClick/BackArrow";
 
 const ViewDish = () => {
   const { id } = useParams(); // Retrieve the id (index) from the URL parameter
+  const navigate = useNavigate();
 
   // Get the dish information based on the id (index)
   const dish = CarouselImageGallery.CarouselImageGallery[Number(id)];
-
+  const handleBackClick = () => {
+    navigate(-1); // Go back to the previous page
+  };
   return (
     <div className="h-[90vh] overflow-scroll no-scrollbar md:h-[100vh] md:w-5/6  mx-auto">
       <div className="p-4">
-        <div className="flex justify-between">
-          <button
-            onClick={() => {
-              closeModal(false);
-            }}
-          >
-            <img src={leftarrow} className="w-4" alt="" />
-          </button>
+        <div className="flex justify-between pb-4">
+        <BackArrow onClick={handleBackClick} />
 
-          <button className="text-sm">clear all</button>
+<img src={fullBookmarkIcon} className="w-4" alt="" />
         </div>
         {dish ? (
           <div>
@@ -36,7 +36,7 @@ const ViewDish = () => {
               />
             </div>
             <div className="overflow-scroll   meda  overscroll-contain  -mt-3">
-              <div className=" bg-lemon-meringue inset-0 p-4 rounded-t-3xl  overflow-y-auto overscroll-contain">
+              <div className=" bg-copper-orange inset-0 p-4 rounded-t-3xl  overflow-y-auto overscroll-contain">
                 <h1 className="text-2xl text-center font-medium">{dish.name}</h1>
 
                 <div className="flex justify-center justify-evenly mt-4 flex-wrap">
