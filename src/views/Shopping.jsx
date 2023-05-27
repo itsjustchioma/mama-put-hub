d862ed9ae5e238324b4d6d9489d79b7f0a3e4d72
Chioma <chiomauchenwosu@gmail.com>
2023-05-27 20:20:28 +0100

    Chioma: Changed font info

--- src/views/Shopping.jsx
+++ src/views/Shopping.jsx
@@ -4,7 +4,7 @@ import ShoppingCategory from "../components/Shopping/ShoppingCategory";
 function Shopping() {
   return (
     <div className="overflow-scroll no-scrollbar h-[90vh]">
-      <h1 className="font-bold text-xl md:text-2xl text-center p-4">
+      <h1 className="font-semibold text-xl md:text-2xl text-center p-4">
         Shopping List
       </h1>
       <ShoppingCategory />
