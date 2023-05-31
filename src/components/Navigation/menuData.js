69484a36422262e0d6e576428d59b4d48cc59218
DavidAjanaku <davidajanaku46163@gmail.com>
2023-05-31 09:16:36 +0100

    done

--- src/components/Navigation/menuData.js
+++ src/components/Navigation/menuData.js
@@ -1,14 +1,14 @@
 export const Menus = [
   {
     name: "Search",
-    icon: "public/assets/search.png",
+    icon: "/assets/search.png",
     dis: "translate-x-0",
     path: "/Home",
     colorName: "pastel-blue",
   },
   {
     name: "Your Library",
-    icon: "public/assets/book.png",
+    icon: "/assets/book.png",
     dis: "translate-x-16",
     altText: "Search Icon",
     path: "/yourlibrary",
@@ -16,7 +16,7 @@ export const Menus = [
   },
   {
     name: "Scanner",
-    icon: "public/assets/scanner.png",
+    icon: "/assets/scanner.png",
     dis: "translate-x-32",
     altText: "Scanner Icon",
     path: "/scanner",
@@ -24,7 +24,7 @@ export const Menus = [
   },
   {
     name: "Shopping",
-    icon: "public/assets/shopping-cart.png",
+    icon: "/assets/shopping-cart.png",
     dis: "translate-x-48",
     altText: "Shopping Cart Icon",
     path: "/shopping",
@@ -32,7 +32,7 @@ export const Menus = [
   },
   {
     name: "Profile",
-    icon: "public/assets/user.png",
+    icon: "/assets/user.png",
     dis: "translate-x-64",
     altText: "User Icon",
     path: "/profile",
