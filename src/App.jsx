3f1f771c02afc41f769450410e712319d780545f
DavidAjanaku <davidajanaku46163@gmail.com>
2023-06-13 00:36:24 +0100

    David

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   const handleLogin = () => {
     setIsVisible(false);
-    localStorage.setItem("isLoggedIn", "false");
+    localStorage.setItem("isLoggedIn", "true");
   };
 
   const handleLogout = () => {
